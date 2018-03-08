35197d43ee041d0ecbc96540d0969cb87ba13fe9
zhaoqinglong <1031432339@qq.com>
2018-03-08 17:29:35 +0800

    Test

---
+++ CLRExer/CLRExer/AttributeDemo/ExcelHeadAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CLRExer.AttributeDemo
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Class)]
+    public class ExcelHeadAttribute : Attribute
+    {
+        /// <summary>
+        /// 设置名称
+        /// </summary>
+        public string _name { get; set; }
+
+        /// <summary>
+        /// 单元格长度
+        /// </summary>
+        public int _length { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Name"></param>
+        /// <param name="Length"></param>
+        public ExcelHeadAttribute(string Name, int Length = 10)
+        {
+            _name = Name;
+            _length = Length;
+        }
+    }
+}
---
+++ CLRExer/CLRExer/AttributeDemo/RiskInfoExcelDto.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CLRExer.AttributeDemo;
+
+
+namespace YTOS.CoalSafe.Application.RiskClosedLoops.Dtos
+{
+    /// <summary>
+    /// 
+    /// </summary>
+   
+    [ExcelHead("隐患信息")]
+    public class RiskInfoExcelDto
+    {
+        /// <summary>
+        /// 隐患编号
+        /// </summary>
+        [ExcelHead("隐患编号")]
+        public string RiskNum { set; get; }
+
+        /// <summary>
+        /// 风险值
+        /// </summary>
+        [ExcelHead("风险值")]
+        public int RiskValue { set; get; }
+
+        /// <summary>
+        /// 分数/区队值
+        /// </summary>
+        [ExcelHead("分数")]
+        public int Score { set; get; }
+
+        /// <summary>
+        /// 风险类型
+        /// </summary>
+        [ExcelHead("风险类型")]
+        public string RiskType { set; get; }
+
+        /// <summary>
+        /// 所属区队
+        /// </summary>
+        [ExcelHead("所属区队")]
+        public string BelongTeam { set; get; }
+
+        /// <summary>
+        /// 隐患名称
+        /// </summary>
+        [ExcelHead("隐患名称")]
+        public string RiskName { set; get; }
+
+        /// <summary>
+        /// 专业
+        /// </summary>
+        [ExcelHead("专业")]
+        public string Profession { set; get; }
+
+        /// <summary>
+        /// 检查人
+        /// </summary>
+        [ExcelHead("检查人")]
+        public string Checker { set; get; }
+
+        /// <summary>
+        /// 发布时间
+        /// </summary>
+        [ExcelHead("发布时间")]
+        public DateTime? PublishTime { set; get; }
+
+        /// <summary>
+        /// 整改状态
+        /// </summary>
+        [ExcelHead("整改状态")]
+        public string CorrectionState { set; get; }
+
+        /// <summary>
+        /// 整改期限
+        /// </summary>
+        [ExcelHead("整改期限")]
+        public string CorrectionLimit { set; get; }
+
+        /// <summary>
+        /// 整改时间
+        /// </summary>
+        [ExcelHead("整改时间")]
+        public DateTime? CorrectionTime { set; get; }
+
+        /// <summary>
+        /// 整改情况
+        /// </summary>
+        [ExcelHead("整改情况")]
+        public string CorrectionDesc { set; get; }
+
+        /// <summary>
+        /// 跟踪复查人
+        /// </summary>
+        [ExcelHead("跟踪复查人")]
+        public string Reviewer { set; get; }
+
+        /// <summary>
+        /// 复查时间
+        /// </summary>
+        [ExcelHead("复查时间")]
+        public DateTime? ReviewTime { set; get; }
+    }
+}
